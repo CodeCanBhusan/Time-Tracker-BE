9c73d89bc9c634dfe82882466fa9f2f31a62d777
Bhusan <bhusan.dotel@nepalcancode.com>
2025-07-04 15:52:28 +0545

    Simplify CORS configuration by using default settings

--- server.js
+++ server.js
@@ -8,23 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "chrome-extension://MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A",
-  "http://localhost:5173",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
